6986556ffaee6068959eed9211f0adf2f570bb91
Joe Lynn <josephlynn94@gmail.com>
2020-04-10 23:42:07 -0400

    added json file to pull project templates from and added images

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import './styles.css'
 
-function Project({ name, image }) {
+function Project({ title, image, desc, skills, link, github}) {
   return (
     <div className="card">
-      <div className="card-img">
-        <img src={image} alt={name} />
+      <div className="card-img_container">
+        <img className="card-img" src={image} alt={title} />
       </div>
       <div className="card-content">
-        <button>View Project</button>
-        <button>View Code</button>
+        <h3>{title}</h3>
+        <p>{desc}</p>
+        <p className="card-skills">{skills}</p>
+        <div className="card-buttons">
+          <a href={link} rel="noopener noreferrer">
+            <button className="btn btn-primary card-btn">View Project</button>
+          </a>
+          <a href={github} rel="noopener noreferrer">
+            <button className="btn btn-secondary card-btn">View Code</button>
+          </a>
+        </div>
       </div>
     </div>
   )
