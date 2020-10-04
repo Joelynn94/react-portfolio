bf6a5e86d7d54df7caa32c0b113227f82e864a8a
Joseph Lynn <josephlynn94@gmail.com>
2020-10-04 18:19:14 -0400

    changed project breakpoints and added stateful netlify form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css'
 
 function Form() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+      )
+      .join('&')
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', name, email, message }),
+    })
+      .then(() => alert('Success!'))
+      .catch((error) => alert(error))
+
+    setName('')
+    setEmail('')
+    setMessage('')
+  }
+
   return (
     <>
       <h1 className='contact-heading'>CONTACT ME.</h1>
-      <form action='' method='post'>
+      <form onSubmit={handleFormSubmit}>
         <fieldset>
           <ul>
             <div className='contact__container'>
@@ -13,6 +40,8 @@ function Form() {
                 <i className='fal fa-user contact__icon'></i>
               </label>
               <input
+                onChange={(e) => setName(e.target.value)}
+                value={name}
                 className='contact__input'
                 type='text'
                 placeholder='e.g. Alex Smith'
@@ -25,6 +54,8 @@ function Form() {
                 <i className='fal fa-envelope contact__icon'></i>
               </label>
               <input
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
                 className='contact__input'
                 type='text'
                 placeholder='e.g. alexsmith@gmail.com'
@@ -37,6 +68,8 @@ function Form() {
                 <i className='fal fa-comment-lines contact__icon'></i>
               </label>
               <textarea
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
                 className='contact__input'
                 type='text'
                 placeholder='Type your message here...'
