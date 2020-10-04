b4b5729102a2535ba25c3dec8edcceca9d68a4a9
Joseph Lynn <josephlynn94@gmail.com>
2020-10-04 18:27:54 -0400

    fixed projects not opening in new tabs

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import './styles.css'
 
-function Project({ title, image, desc, skills, link, github}) {
+function Project({ title, image, desc, skills, link, github }) {
   return (
-    <div className="card">
-      <div className="card-img_container">
-        <img className="card-img" src={image} alt={title} />
+    <div className='card'>
+      <div className='card-img_container'>
+        <img className='card-img' src={image} alt={title} />
       </div>
-      <div className="card-content">
+      <div className='card-content'>
         <h3>{title}</h3>
         <p>{desc}</p>
-        <p className="card-skills">{skills}</p>
-        <div className="card-buttons">
-          <a href={link} rel="noopener noreferrer">
-            <button className="btn btn-primary card-btn">View Project</button>
+        <p className='card-skills'>{skills}</p>
+        <div className='card-buttons'>
+          <a href={link} rel='noopener noreferrer' target='_blank'>
+            <button className='btn btn-primary card-btn'>View Project</button>
           </a>
-          <a href={github} rel="noopener noreferrer">
-            <button className="btn btn-secondary card-btn">View Code</button>
+          <a href={github} rel='noopener noreferrer' target='_blank'>
+            <button className='btn btn-secondary card-btn'>View Code</button>
           </a>
         </div>
       </div>
