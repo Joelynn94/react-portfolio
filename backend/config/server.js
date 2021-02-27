99e784f756f26695ea06f9ced612a2d47570b950
Joseph Lynn <josephlynn94@gmail.com>
2021-02-27 17:50:53 -0500

    setup Strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2b8022c38e897c373cc7e67cd7def6cc'),
+    },
+  },
+});
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'df3e43ca-0692-4b5a-bbd8-f60a78d51182'
-};
